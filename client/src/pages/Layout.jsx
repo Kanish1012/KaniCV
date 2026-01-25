1083ddd59e9a02427559f7955baeb4aa886d02d0
Kanish <kanishv1012@gmail.com>
2026-01-25 20:37:42 +0530

    Refactor routing and layout structure

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return (
-    <div>Layout Page</div>
-  )
-}
+    return (
+        <div>
+            <h1>Layout Page</h1>
+            <div>
+                <Outlet />
+            </div>
+        </div>
+    );
+};
 
-export default Layout
+export default Layout;
