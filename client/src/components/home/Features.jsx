872b8a0384d9dbe21d00949654dc7927f659de2d
Kanish <kanishv1012@gmail.com>
2026-01-25 22:02:01 +0530

    udpated features.jsx

--- client/src/components/home/Features.jsx
+++ client/src/components/home/Features.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Zap } from "lucide-react";
+import Title from "./Title";
 
 const Features = () => {
     const [isHover, setIsHover] = React.useState(false);
@@ -9,10 +10,14 @@ const Features = () => {
             className="flex flex-col items-center my-10 scroll-mt-12"
         >
             <div className="flex items-center gap-2 text-sm text-green-600 bg-green-400/10 rounded-full px-6 py-1.5">
-                <Zap width={14} />               
+                <Zap width={14} />
                 <span>Simple process</span>
             </div>
-            <div className="flex flex-col md:flex-row items-center justify-center">
+            <Title
+                title="Build your resume"
+                description="Our streamlined process helps you create a professional resume in minutes with intelligent AI-powered tools and features."
+            />
+            <div className="flex flex-col md:flex-row items-center justify-center xl-mt-10">
                 <img
                     className="max-w-2xl w-full xl:-ml-32"
                     src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/features/group-image-1.png"
