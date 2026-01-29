e3998daef4cfc65fb4cd77644e1dd004cd831967
Kanish <kanishv1012@gmail.com>
2026-01-29 19:59:25 +0530

    remove background

--- client/src/components/PersonalInfoForm.jsx
+++ client/src/components/PersonalInfoForm.jsx
@@ -47,6 +47,27 @@ const PersonalInfoForm = ({
                         }
                     />
                 </label>
+
+                {typeof data.image === "object" && (
+                    <div className="flex flex-col gap-1 pl-4 text-sm">
+                        <p>Remove Background</p>
+
+                        <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
+                            <input
+                                type="checkbox"
+                                className="sr-only peer"
+                                onChange={() =>
+                                    setRemoveBackground((prev) => !prev)
+                                }
+                                checked={removeBackground}
+                            />
+
+                            <div className="w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-600 transition-colors duration-200" />
+
+                            <span className="dot absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-4" />
+                        </label>
+                    </div>
+                )}
             </div>
         </div>
     );
