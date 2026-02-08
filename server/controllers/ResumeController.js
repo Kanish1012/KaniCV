75adebacc23f85000b1be6d87900c58545345fab
Kanish <kanishv1012@gmail.com>
2026-02-08 22:31:07 +0530

    added deleteResume controllers

--- server/controllers/ResumeController.js
+++ server/controllers/ResumeController.js
@@ -1,6 +1,5 @@
 import Resume from "../models/Resume.js";
 
-
 // controller for creating resume
 // post: /api/resumes/create
 export const createResume = async (req, res) => {
@@ -10,12 +9,27 @@ export const createResume = async (req, res) => {
 
         const newResume = await Resume.create({ userId, title });
 
-        return res
-            .status(201)
-            .json({
-                message: "Resume created successfully",
-                resume: newResume,
-            });
+        return res.status(201).json({
+            message: "Resume created successfully",
+            resume: newResume,
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+// controller for deleting resumes
+// delete: /api/resumes/delete
+export const deleteResume = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const { resumeId } = req.params;
+
+        await Resume.findOneAndDelete({ userId, _id: resumeId });
+
+        return res.status(204).json({
+            message: "Resume deleted successfully",
+        });
     } catch (error) {
         return res.status(500).json({ message: "Server error" });
     }
