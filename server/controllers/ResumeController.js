b9a2ac33c9ecd806ddd31020801956a1d6007df4
Kanish <kanishv1012@gmail.com>
2026-02-09 16:50:25 +0530

    add updateResume controller and integrate ImageKit for image uploads; add multer configuration

--- server/controllers/ResumeController.js
+++ server/controllers/ResumeController.js
@@ -1,4 +1,6 @@
+import imageKit from "../config/imageKit.js";
 import Resume from "../models/Resume.js";
+import fs from "fs";
 
 // controller for creating resume
 // post: /api/resumes/create
@@ -73,6 +75,57 @@ export const getPublicResumeById = async (req, res) => {
         }
 
         return res.status(200).json({ resume });
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+// controller for updating a resume
+// PUT: /api/resumes/update
+
+export const updateResume = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const { resumeId, resumeData, removeBackground } = req.body;
+        const image = req.file;
+
+        let resumeDataCopy;
+        try {
+            resumeDataCopy = JSON.parse(resumeData);
+        } catch {
+            return res.status(400).json({ message: "Invalid resume data" });
+        }
+
+        if (image) {
+            const imageBufferData = fs.createReadStream(image.path);
+            const response = await imageKit.files.upload({
+                file: imageBufferData,
+                fileName: "resume.png",
+                folder: "user-resumes",
+                transformation: {
+                    pre:
+                        "w-300, h-300, fo-face, z-0.75" +
+                        (removeBackground ? ",e-bgremove" : ""),
+                },
+            });
+
+            resumeDataCopy.personal_info.image = response.url;
+        }
+
+        const resume = await Resume.findOneAndUpdate(
+            {
+                userId,
+                _id: resumeId,
+            },
+            resumeDataCopy,
+            {
+                new: true,
+            },
+        );
+
+        return res
+            .status(200)
+            .json({ message: "Updated successfully", resume });
     } catch (error) {
         return res.status(500).json({ message: "Server error" });
     }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {
     getUserId,
-    getuserResumes,
+    getUserResumes,
     loginUser,
     registerUser,
 } from "../controllers/UserController.js";
@@ -12,6 +12,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserId);
-userRouter.get("/resumes", protect, getuserResumes);
+userRouter.get("/resumes", protect, getUserResumes);
 
 export default userRouter;
