89af11e09b1c5f56937a4d853a1dec0fcdd0a797
Kanish <kanishv1012@gmail.com>
2026-02-09 13:13:04 +0530

    add getResumeById and getPublicResumeById controllers for fetching resumes

--- server/controllers/ResumeController.js
+++ server/controllers/ResumeController.js
@@ -34,3 +34,46 @@ export const deleteResume = async (req, res) => {
         return res.status(500).json({ message: "Server error" });
     }
 };
+
+//get user resume by id
+// GET: /api/resumes/get
+export const getResumeById = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const { resumeId } = req.params;
+
+        const resume = await Resume.findOne(
+            { _id: resumeId, userId: req.userId },
+            "-__v -createdAt -updatedAt",
+        );
+
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found" });
+        }
+
+        return res.status(200).json({ resume });
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+// get resume by id public
+// GET: /api/resumes/public/:resumeId
+
+export const getPublicResumeById = async (req, res) => {
+    try {
+        const { resumeId } = req.params;
+        const resume = await Resume.findOne(
+            { _id: resumeId, public: true },
+            "-__v -updatedAt -userId",
+        );
+
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found" });
+        }
+
+        return res.status(200).json({ resume });
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
+};
